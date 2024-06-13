7cac1ab9750ffccd8d09a033431db90d4488ee79
Mex93 <ryasanow@mail.ru>
2024-06-13 16:04:53 +0300

    Шаблоны сканировки - сделал удаление

--- page_templates/routes/find.py
+++ page_templates/routes/find.py
@@ -16,6 +16,9 @@ from engine.sql.CSQL import NotConnectToDB, ErrorSQLQuery, ErrorSQLData
 from engine.sql.sql_data import SQL_TV_MODEL_INFO_FIELDS
 from engine.tv_models.CModels import CModels
 
+from engine.users.users_log.CSQLUserLogQuerys import CSQLUserLogQuerys
+from engine.users.users_log.enums import LOG_TYPE, LOG_SUBTYPE, LOG_OBJECT_TYPE
+
 cdebug = CDebug()
 cdebug.debug_system_on(True)
 
@@ -38,37 +41,51 @@ def templates_delete_mask_ajax(scan_fk, model_id, model_name):
         if result_connect is True:
             data = csql.is_valid_scanned_mask(scan_fk, model_id)
             if data is not False:
+                if csql.delete_template(scan_fk, model_id) is True:
 
-                response_for_client.update({"error_text": f"Шаблон '{model_name}' успешно удалён!"})
-                response_for_client.update({"result": True})
+                    #################################
+                    text = f"Пользователь ID: [{account_name}[{account_idx}]] удалил шаблон сканировки '{model_name}'[MID: {model_id}, SID: {scan_fk}]"
+                    CSQLUserLogQuerys.send_log(
+                        account_idx,
+                        LOG_OBJECT_TYPE.LGOT_USER,
+                        LOG_TYPE.LGT_SCAN_TEMPLATE,
+                        LOG_SUBTYPE.LGST_DELETE,
+                        text)
+
+                    response_for_client.update({"error_text": f"Шаблон '{model_name}' успешно удалён!"})
+                    response_for_client.update({"result": True})
+                else:
+                    response_for_client.update(
+                        {"error_text": f"Ошибка удаления шаблона '{model_name}[{model_id}]'!"})
+                    response_for_client.update({"result": False})
             else:
                 response_for_client.update({"error_text": f"Не найдена маска сканировки для '{model_name}[{model_id}]'!"})
                 response_for_client.update({"result": False})
         else:
             raise NotConnectToDB("Not SQL Connect!")
     except NotConnectToDB as err:
-        response_for_client.update({"error_text": "errorcode: templates_edit_mask_ajax -> [NotConnectToDB]"})
+        response_for_client.update({"error_text": "errorcode: templates_delete_mask_ajax -> [NotConnectToDB]"})
         cdebug.debug_print(
-            f"templates_edit_mask_ajax AJAX -> [Получение списка сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
+            f"templates_delete_mask_ajax AJAX -> [Удаление шаблона сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
             f"[Исключение] [NotConnectToDB: '{err}']")
 
     except ErrorSQLQuery as err:
-        response_for_client.update({"error_text": "errorcode: templates_edit_mask_ajax -> [ErrorSQLQuery]"})
+        response_for_client.update({"error_text": "errorcode: templates_delete_mask_ajax -> [ErrorSQLQuery]"})
         cdebug.debug_print(
-            f"templates_edit_mask_ajax AJAX -> [Получение списка сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
+            f"templates_delete_mask_ajax AJAX -> [Удаление шаблона сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
             f"[Исключение] [ErrorSQLQuery: '{err}']")
 
     except ErrorSQLData as err:
-        response_for_client.update({"error_text": "errorcode: templates_edit_mask_ajax -> [ErrorSQLData]"})
+        response_for_client.update({"error_text": "errorcode: templates_delete_mask_ajax -> [ErrorSQLData]"})
         cdebug.debug_print(
-            f"templates_edit_mask_ajax AJAX -> [Получение списка сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
+            f"templates_delete_mask_ajax AJAX -> [Удаление шаблона сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
             f"[Исключение] [ErrorSQLData: '{err}']")
 
     except Exception as err:
 
-        response_for_client.update({"error_text": "errorcode: templates_edit_mask_ajax -> [Error Data]"})
+        response_for_client.update({"error_text": "errorcode: templates_delete_mask_ajax -> [Error Data]"})
         cdebug.debug_print(
-            f"templates_edit_mask_ajax AJAX -> [Получение списка сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
+            f"templates_delete_mask_ajax AJAX -> [Удаление шаблона сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
             f"[Исключение] [Error Data: '{err}']")
 
     finally:
@@ -76,7 +93,7 @@ def templates_delete_mask_ajax(scan_fk, model_id, model_name):
 
     result = json.dumps(response_for_client)
     cdebug.debug_print(
-        f"templates_edit_mask_ajax AJAX -> [Получение списка сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
+        f"templates_delete_mask_ajax AJAX -> [Удаление шаблона сканировки '{model_name}[{model_id}]'] -> [IDX:{account_idx}, {account_name}] -> "
         f"[Ответ в JS] -> [{count}]")
     return result
 
