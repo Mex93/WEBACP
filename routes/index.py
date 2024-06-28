0e3742b532aa93e34aec926858a3c329a10ecaf7
Mex93 <ryasanow@mail.ru>
2024-06-28 16:08:04 +0300

    control panel alpha

--- routes/index.py
+++ routes/index.py
@@ -4,7 +4,6 @@ from engine.pages.enums import PAGE_ID
 from engine.pages.CPages import CPages
 from engine.debug.CDebug import CDebug
 
-
 cdebug = CDebug()
 cdebug.debug_system_on(True)
 
@@ -17,6 +16,12 @@ name = cpages.get_page_route_name_from_page_id(PAGE_ID.INDEX)
 @app.route('/')
 @app.route('/home')
 def index():
+    CURRENT_YEAR = "2024"
+    CURRENT_ADMIN_EMAIL = "ryazanov.n@tvkvant.ru"
+    CURRENT_TEXT = (
+        '<span class = "header">Сайт для редактирования базы данных сборочного производства.</span><br><br>'
+        '<span class = "text">Все права принадлежат ООО Квант.<br>'
+        'Разработчик: Рязанов НВ<br>'
+        f'По всем интересующим вопросам пишите на почту </span><span class = "email">{CURRENT_ADMIN_EMAIL}</span><br>')
 
-    return cpages.set_render_page(PAGE_ID.INDEX)
-# cpages.set_render_page(PAGE_ID.INDEX, session_unit=session_unit)
+    return cpages.set_render_page(PAGE_ID.ABOUT, year=CURRENT_YEAR, text_main=CURRENT_TEXT)
